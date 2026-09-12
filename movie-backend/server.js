217552e421c36f377253d280753adfc5ecaf2dfe
khansaddam93667-cloud <khansaddam93667@gmail.com>
2026-09-12 20:18:45 +0530

    update movie-backend/server.js

--- movie-backend/server.js
+++ movie-backend/server.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const movieRoutes = require('./routes/movies');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/movies', movieRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Movie Rating API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const cors = require('cors');
 const dotenv = require('dotenv');
 const movieRoutes = require('./routes/movies');
 
